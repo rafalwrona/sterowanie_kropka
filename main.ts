eac0ad8589653c48d9feceebd79c6bade2ff3b06
rafalwrona <wronarafal@gmail.com>
2022-01-28 12:41:51 +0100

    Aktualizacja pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    Lilo.move(-1)
-})
-input.onButtonPressed(Button.AB, function () {
-    Lilo.turn(Direction.Right, 90)
-})
-input.onButtonPressed(Button.B, function () {
-    Lilo.move(1)
+    if (Lilo.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.gameOver()
+    }
 })
 let Lilo: game.LedSprite = null
 Lilo = game.createSprite(2, 2)
+game.setScore(0)
+basic.forever(function () {
+    Lilo.move(1)
+    Lilo.ifOnEdgeBounce()
+    if (game.score() < 5) {
+        basic.pause(700)
+    } else if (game.score() >= 5 && game.score() < 10) {
+        basic.pause(500)
+    } else if (game.score() >= 10 && game.score() < 20) {
+        basic.pause(400)
+    } else {
+        basic.pause(300)
+    }
+})
